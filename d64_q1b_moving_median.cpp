a3d946780ff3bdfa67bc8429f12eed5817c8c1fe
Pana Wanitchollakit <pnaw.wan@gmail.com>
2023-08-28 09:39:45 +0700

    Fix style

--- d64_q1b_moving_median.cpp
+++ d64_q1b_moving_median.cpp
@@ -1,57 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+multiset<int> s;
+queue<int> q;
 int main() {
 	ios_base::sync_with_stdio(false), cin.tie(nullptr);
 	int n, w;
 	cin >> n >> w;
-	// w--;  // for CSES Sliding Median
-	multiset<int> s;
-	queue<int> q;
 	for(int i = 0, x; i < w + 1; i++) {
 		cin >> x;
-		q.push(x);
-		s.insert(x);
+		q.push(x), s.insert(x);
 	}
-	auto it = s.begin();
-	for(int i = 0; i < w / 2; i++)
-		it++;
+	auto it = next(s.begin(), w/2); // median start at w/2+1
 	cout << *it << " ";
 	for(int i = w + 1, x; i < n; i++) {
 		cin >> x;
 		q.push(x);
 		int del = q.front();
 		q.pop();
-		if(del == x) {
+		if(del == x) {	// remove and add same element
 			cout << *it << " ";
 			continue;
 		}
-		auto now = s.upper_bound(del);
-		now--;
-		if(now == it) {	 // *it == del && has only *it in mset
+		auto now = --s.upper_bound(del); // find the right most element to delete
+		if(now == it) {	 // no duplicate median && remove it
 			s.insert(x);
-			if(x > *it)
-				it++;
-			else
-				it--;
+			if(x > *it) it++;
+			else it--;
 			s.erase(now);
+			cout << *it << " ";
+			continue;
 		}
-		else if((x < *it && del < *it) || (x >= *it && del >= *it)) {
-			s.insert(x);
-			s.erase(now);
-		}
-		else if(x < *it && del >= *it) {
-			s.insert(x);
-			s.erase(now);
-			it--;
-		}
-		else if(x >= *it && del < *it) {
-			s.insert(x);
-			s.erase(now);
-			it++;
-		}
-		else
-			assert(false);
+		s.insert(x);
+		s.erase(now);
+		// remove either both left side or right side the median => the median won't move
+		if(x < *it && del >= *it) it--;
+		else if(x >= *it && del < *it) it++;
 		cout << *it << " ";
 	}
 	cout << "\n";
