a0e1602f139e8d19f294d35f6371109ccaf92d14
Pana Wanitchollakit <pnaw.wan@gmail.com>
2023-09-06 10:33:52 +0700

    Top 10 Idiot Pana Moment

---
+++ ex00m1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int n,m;
+    cin >> n >> m;
+    pqg<pair<int,int>> pq;
+    // for(int i=0, x; i<n; i++) {
+    //     cin >> x;
+    //     pq.emplace(x, x);
+    //     cout << 0 << "\n";
+    // }
+    for(int i=0, fin, x; i<m; i++) {
+        if(i < n) {
+            cin >> x;
+            pq.push(x, x);
+            cout << "0\n";
+            continue;
+        }
+        tie(fin, x) = pq.top();
+        pq.pop();
+        cout << fin << "\n";
+        pq.emplace(fin+x, x);
+    }
+    return 0;
+}
