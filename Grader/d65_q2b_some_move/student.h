6c891cda44bb1840bc9c495f6ceed83c48c6b121
Pana Wanitchollakit <pnaw.wan@gmail.com>
2023-09-20 00:56:17 +0700

    New Solution for vector_some_move new testcases

--- Grader/d65_q2b_some_move/student.h
+++ Grader/d65_q2b_some_move/student.h
@@ -1,29 +1,23 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
 
-#include "vector-sm.h"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-#include<cassert>
-
+#include"vector-sm.h"
+#include<bits/stdc++.h>
+#define sz(x) (int)(x).size()
+int c=1875;
 template <typename T>
 void CP::vector_some_move<T>::insert(int index, std::vector<T> &value) {
-	using namespace std;
-	int i = upper_bound(aux.begin(), aux.end(), index) - aux.begin();
-	assert(i!=0); //bruh -> not exist got [PPP](100) ???
-	if(i>0) index -= aux[i-1];
-	vector<vector<T>> ins;
-	vector<T>& v=mData[i];
-	if(index) ins.push_back(vector<T>(v.begin(), v.begin()+index)), aux.push_back(0);
-	ins.push_back(value);
-	if(index) ins.push_back(vector<T>(v.begin()+index, v.end())), mData.erase(mData.begin()+i);
-	mData.insert(mData.begin() + i, ins.begin(), ins.end()), aux.push_back(0);
-
-	for(; i<mData.size(); i++) aux[i] = (i>0?aux[i-1]:0) + mData[i].size();
-	mSize = mCap = aux.back();
-
+	int i = std::upper_bound(aux.begin(), aux.end(), index)-aux.begin();
+	i?index-=aux[i-1]:0;
+	if(i==sz(mData)) mData.push_back(value);
+	else {
+		mData[i].insert(mData[i].begin()+index, value.begin(), value.end());
+		if(sz(mData[i]) > c) {
+			mData.insert(mData.begin()+i+1, std::vector<T>(mData[i].begin()+c, mData[i].end()));
+			while(sz(mData[i])>c) mData[i].pop_back();
+		}
+	}
+	for(aux.resize(sz(mData)); i<sz(mData); i++) aux[i] = (i?aux[i-1]:0) + sz(mData[i]);
+	mSize=mCap=aux.back();
 }
-
 #endif
